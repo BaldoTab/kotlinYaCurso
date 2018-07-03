1b97e803b69bfb4aca7531f26d11bcd032cda62d
Baldo Taberner <taberneraguas@gmail.com>
2018-07-03 22:07:08 +0200

    Add For And Ranges

---
+++ src/Main_Ranges.kt
@@ -0,0 +1,21 @@
+
+fun main(params: Array<String>) {
+    val digit = 1..9
+    val dozen = 1..12
+    var letras = "a".."z"
+
+    println(5 in dozen) // true
+    println(13 in dozen) // false
+
+    for (i in digit) {
+        println(i)
+    }
+
+    for (i in 1..9 step 2) {
+        println(i)
+    }
+
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+}
