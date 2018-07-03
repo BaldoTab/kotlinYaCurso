72c14bf0c28f18737e3a7834a9410a103ab9a1eb
Baldo Taberner <taberneraguas@gmail.com>
2018-07-03 22:42:05 +0200

    Add Arrays Examples

---
+++ src/Main_Arrays.kt
@@ -0,0 +1,34 @@
+fun main(params: Array<String>) {
+    /*
+    IntArray
+    ByteArray
+    ShortArray
+    LongArray
+    FloatArray
+    DoubleArray
+    BooleanArray
+    CharArray
+    Array<String>
+     */
+
+    val salaries: IntArray
+    salaries = IntArray(5)
+
+    //carga de sus elementos por teclado
+    for(i in 0..4) {
+        print("Ingrese sueldo:")
+        salaries[i] = readLine()!!.toInt()
+    }
+    //impresion de sus elementos
+    for(i in 0..salaries.size - 1) {
+        println(salaries[i])
+    }
+
+    for (salary in salaries) {
+        println(salary)
+    }
+
+    for (i in salaries.indices) { // 0..4
+        println(i)
+    }
+}
