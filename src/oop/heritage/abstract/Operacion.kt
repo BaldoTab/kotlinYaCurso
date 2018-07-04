5373ade7cf944c94e48bf7d655daefe359c27a01
Baldo Taberner <taberneraguas@gmail.com>
2018-07-04 22:53:42 +0200

    Add spanish explanation in Abstract class

--- src/oop/heritage/abstract/Operacion.kt
+++ src/oop/heritage/abstract/Operacion.kt
@@ -1,5 +1,12 @@
 package oop.heritage.abstract
 
+/*
+En algunas situaciones tenemos métodos y propiedades comunes a un conjunto de clases,
+podemos agrupar dichos métodos y propiedades en una clase abstracta.
+
+No se pueden definir objetos de una clase abstracta
+seguramente será heredada por otras clases de las que si podremos definir objetos.
+ */
 abstract class Operacion(val valor1: Int, val valor2: Int) {
     protected var resultado: Int = 0
 
